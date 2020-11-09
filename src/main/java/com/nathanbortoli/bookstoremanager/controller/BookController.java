712450bb804f24d155eebbbc941d91eadb361844
Nathan Bortoli <nathanbortoli@gmail.com>
2020-11-09 18:25:27 -0300

    Develped get by id operation

--- src/main/java/com/nathanbortoli/bookstoremanager/controller/BookController.java
+++ src/main/java/com/nathanbortoli/bookstoremanager/controller/BookController.java
@@ -6,10 +6,7 @@ import com.nathanbortoli.bookstoremanager.entity.Book;
 import com.nathanbortoli.bookstoremanager.reposity.BookRepository;
 import com.nathanbortoli.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,5 +24,10 @@ public class BookController {
     @PostMapping
     public MessageResponseDTO create (@RequestBody @Valid BookDTO bookDTO){
         return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id){
+        return bookService.findById(id);
     }
 }
--- src/main/java/com/nathanbortoli/bookstoremanager/service/BookService.java
+++ src/main/java/com/nathanbortoli/bookstoremanager/service/BookService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Optional;
+
 @Service
 public class BookService {
 
@@ -27,5 +29,11 @@ public class BookService {
 
         Book saveBook = bookRepository.save(bookToSabe);
         return MessageResponseDTO.builder().message("Book created with ID: " + saveBook.getId()).build();
+    }
+
+
+    public BookDTO findById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return bookMapper.toDTO(optionalBook.get());
     }
 }
